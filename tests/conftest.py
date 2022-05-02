2fbe463894c84637f414540ab718e786427a1bbe
charles <cle4@njit.edu>
2022-05-02 02:35:17 -0400

    Project 3 tweaks

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,35 @@
 """This makes the test configuration setup"""
 # pylint: disable=redefined-outer-name
-
+import os
 import pytest
-from app import create_app
+from app import create_app, User
+from app.db import db
 
 
 @pytest.fixture()
 def application():
     """This makes the app"""
-    application = create_app()
-    application.config.update({
-        "TESTING": True,
-    })
-    yield application
+    os.environ['FLASK_ENV'] = 'testing'
 
+    application = create_app()
+
+    with application.app_context():
+        db.create_all()
+        yield application
+        db.session.remove()
+
+@pytest.fixture()
+def add_user(application):
+    """ Adding a user to the application's database """
+    with application.app_context():
+        user = User('keith@webizly.com', 'testtest', 0)
+        db.session.add(user)
+        db.session.commit()
 
 @pytest.fixture()
 def client(application):
     """This makes the http client"""
     return application.test_client()
-
 
 @pytest.fixture()
 def runner(application):
