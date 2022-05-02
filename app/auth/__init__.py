39ea91fe9155344ba70d68f99a7741998a9895c5
CharlesEsp <73568176+CharlesEsp@users.noreply.github.com>
2022-05-02 00:27:37 -0400

    base tweaks

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -7,7 +7,7 @@ from werkzeug.security import generate_password_hash
 from app.auth.decorators import admin_required
 from app.auth.forms import login_form, register_form, profile_form, security_form, user_edit_form, create_user_form
 from app.db import db
-from app.db.models import User, Location, location_user
+from app.db.models import User, Location, user_location
 from flask_mail import Message
 
 auth = Blueprint('auth', __name__, template_folder='templates')
