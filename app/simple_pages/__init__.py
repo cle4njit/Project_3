123cebf510ee475dda88cc19c14ed83b7461c06e
charles <cle4@njit.edu>
2022-05-01 23:04:04 -0400

    Project 3 tweaks

--- app/simple_pages/__init__.py
+++ app/simple_pages/__init__.py
@@ -2,10 +2,22 @@ from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
 simple_pages = Blueprint('simple_pages', __name__,
-                        template_folder='templates',static_folder='static')
+                        template_folder='templates')
 
+@simple_pages.route('/')
+def index():
+    try:
+        return render_template('index.html')
+    except TemplateNotFound:
+        abort(404)
 
-@simple_pages.route('/', defaults={'page': 'index'})
+@simple_pages.route('/about')
+def about():
+    try:
+        return render_template('about.html')
+    except TemplateNotFound:
+        abort(404)
+
 @simple_pages.route('/<page>')
 def show(page):
     try:
--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -8,6 +8,10 @@ def test_request_main_menu_links(client):
     assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
     assert b'<a class="nav-link" href="/page3">Python/Flask</a>' in response.data
     assert b'<a class="nav-link" href="/page4">CI/CD</a>' in response.data
+    assert b'<a class="nav-link" href="/page5">Pylint</a>' in response.data
+    assert b'<a class="nav-link" href="/page6">AAA Testing</a>' in response.data
+    assert b'<a class="nav-link" href="/page7">OOPs</a>' in response.data
+    assert b'<a class="nav-link" href="/page8">SOLID</a>' in response.data
 
 
 
@@ -41,7 +45,33 @@ def test_request_page4(client):
     assert response.status_code == 200
     assert b"Continuous Delivery / CD: " in response.data
 
-def test_request_page_not_found(client):
+
+def test_request_page5(client):
     """This makes the index page"""
     response = client.get("/page5")
+    assert response.status_code == 200
+    assert b"Pylint: " in response.data
+
+
+def test_request_page6(client):
+    """This makes the index page"""
+    response = client.get("/page6")
+    assert response.status_code == 200
+    assert b"Arrange: " in response.data
+
+def test_request_page7(client):
+    """This makes the index page"""
+    response = client.get("/page7")
+    assert response.status_code == 200
+    assert b"Instantiation: " in response.data
+
+def test_request_page8(client):
+    """This makes the index page"""
+    response = client.get("/page8")
+    assert response.status_code == 200
+    assert b"Simple Calculator: " in response.data
+
+def test_request_page_not_found(client):
+    """This makes the index page"""
+    response = client.get("/page9")
     assert response.status_code == 404
